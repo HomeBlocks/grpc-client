34f1c498aae3d882ed3856ce05eb6acd743928ad
Влад <119223879+DeshSkk@users.noreply.github.com>
2025-02-09 14:50:13 +0500

    Реализовать функционал gRPC клиента #1 (#2)
    * init
    
    * init
    
    * init
    
    ---------
    
    Co-authored-by: Vlad Bulavin <bulavin_vm@ekb.sima-land.ru>

---
+++ client.go
@@ -0,0 +1,48 @@
+package client
+
+import (
+	"context"
+
+	"github.com/pkg/errors"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+type Config struct {
+	Address string
+}
+
+type Client struct {
+	config Config
+	conn   *grpc.ClientConn
+}
+
+func (c *Client) OnStart(_ context.Context) error {
+	conn, err := grpc.NewClient(c.config.Address, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		return errors.Wrap(err, "failed to create grpc client")
+	}
+
+	c.conn = conn
+
+	return nil
+}
+
+func (c *Client) OnStop(_ context.Context) error {
+	err := c.conn.Close()
+	if err != nil {
+		return errors.Wrap(err, "failed to close grpc client")
+	}
+
+	return nil
+}
+
+func (c *Client) Connection() *grpc.ClientConn {
+	return c.conn
+}
+
+func NewClient(c Config) *Client {
+	return &Client{
+		config: c,
+	}
+}
